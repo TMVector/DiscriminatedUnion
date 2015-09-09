d0b29b02996bf42c92d7f08b3ac2ac43257ffaa1
TMVector <tmvector@gmail.com>
2015-09-09 14:24:43 +0100

    Add example project

---
+++ TMV.DiscriminatedUnion.Example/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TMV.DiscriminatedUnion.Example
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Vehicle vehicle = new Vehicle(Bike.Instance);
+
+			int wheels = vehicle.Match(
+				(Bike b)	=> 2,
+				(Car c)		=> 4,
+				(Lorry l)	=> l.Weight < 18 ? 4 : 12
+			);
+
+			Console.WriteLine("A bike has {0} wheels", wheels);
+		}
+	}
+}
---
+++ TMV.DiscriminatedUnion.Example/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TMV.DiscriminatedUnion.Example
+{
+	class Bike { public static readonly Bike Instance = new Bike(); }
+	class Car { public static readonly Car Instance = new Car(); }
+	class Lorry
+	{
+		private readonly double _Weight;
+		public double Weight { get { return _Weight; } }
+		public Lorry(double weight)
+		{
+			_Weight = weight;
+		}
+	}
+
+	class Vehicle : Union<Bike, Car, Lorry>
+	{
+		public Vehicle(Bike bike) : base(bike) { }
+		public Vehicle(Car car) : base(car) { }
+		public Vehicle(Lorry lorry) : base(lorry) { }
+	}
+}
